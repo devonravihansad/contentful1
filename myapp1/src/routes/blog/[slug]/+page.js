bdff58cc1cb1efeef72174a921d1ef5d6b3f042f
devonravihansad <devonravihansad@gmail.com>
2025-08-21 07:50:54 +0000

    update 3

--- myapp1/src/routes/blog/[slug]/+page.js
+++
@@ -1,6 +0,0 @@
-export function load({ params }) {
-  return {
-    title: "My Blog Post",
-    slug: params.slug,
-  };
-}
--- myapp1/vite.config.ts
+++ myapp1/vite.config.ts
@@ -1,9 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['370f35a0-228f-46fa-ba45-68096e555006-00-vrglenih859c.sisko.replit.dev']
-	}
+		allowedHosts: [
+			"370f35a0-228f-46fa-ba45-68096e555006-00-vrglenih859c.sisko.replit.dev",
+		],
+	},
 });
